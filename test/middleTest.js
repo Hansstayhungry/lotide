361436caaec3fdc7534ef91278529aa7bc3b33ec
Hans Wang <hansmikaa@live.cn>
2023-03-23 21:01:33 -0400

    add index.js

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,5 +1,9 @@
-const middle = require('../middle.js')
-;const assertArraysEqual = require('../assertArraysEqual.js');
+const assert = require('chai').assert;
+const middle = require('../middle.js');
 
-//test
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+
+describe("#middle", () => {
+  it("returns 3 for [1, 2, 3, 4, 5]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
+  });
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,12 +1,19 @@
-const tail = require('../tail');
-const assertEqual = require('../assertEqual');
-
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-(tail(words)); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
-
+const assert = require('chai').assert;
+const tail   = require('../tail');
 const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs1"); // ensure second element is "Labs"
+
+describe("#tail", () => {
+  it("ensure we get back two elements", () => {
+    assert.strictEqual(result.length, 2);
+  });
+
+  it("ensure first element is 'Lighthouse'", () => {
+    assert.strictEqual(result[0], "Lighthouse"); 
+  });
+
+  it("ensure second element is 'Labs'", () => {
+    assert.strictEqual(result[1], "Labs"); 
+  });
+
+});
+
